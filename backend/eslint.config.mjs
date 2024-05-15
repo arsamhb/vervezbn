1004234fc11b237be50f92f7ea50bea670d9b073
Rsam <arsam.hb@gmail.com>
2024-05-15 18:28:38 +0330

    feat/husky configed test commit

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,10 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint-define-config';
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default defineConfig({
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off', // Ignoring any type errors
+    '@typescript-eslint/no-unused-vars': 'off', // Ignoring unused variables
+    '@typescript-eslint/no-var-requires': 'off', // Ignoring require statements not part of import
+    'prefer-const': 'off', // Ignoring const usage preference
+  }
+});
