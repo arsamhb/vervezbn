4676c099c4ff9b413fc2efe9c37acd0233b15625
Arsam <87975732+arsamhb@users.noreply.github.com>
2024-07-04 18:52:55 +0330

    feat/transaction controller

---
+++ backend/src/controllers/transaction/transaction.contorller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { createTransaction } from "../../repositories/transaction.repository";
+import {
+  getWalletByUserId,
+  updateWalletBalance,
+} from "../../repositories/wallet.repositry";
+
+export const addCoins = async (req: Request, res: Response) => {
+  const userId = req.body.user.id; // write a middleware to manage this ????
+  const { amount } = req.body;
+
+  try {
+    const wallet = await getWalletByUserId(userId);
+    if (!wallet)
+      return res
+        .status(500)
+        .json({ message: "Something happened please try again later" });
+
+    await updateWalletBalance(wallet.id, amount);
+    await createTransaction(amount, "CREDIT", wallet.id);
+
+    res.status(200).json({ message: "Coins added successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
