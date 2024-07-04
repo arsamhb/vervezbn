9ab72df4ca57bad76500d89a8a65c83cffb3d464
Arsam <87975732+arsamhb@users.noreply.github.com>
2024-07-04 17:56:05 +0330

    fix/refreshToken controller

--- backend/src/controllers/auth/refreshTokenController.ts
+++ backend/src/controllers/auth/refreshTokenController.ts
@@ -1,57 +1,40 @@
-// import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
-// import { User } from "../types/user";
-// import * as dotenv from "dotenv";
+import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
 import { Response, Request } from "express";
-
-// const userDB = {
-//   users: require("../models/users.json"),
-//   setUsers: function (data: Array<User>) {
-//     this.users = data;
-//   },
-// };
 
 export const handleRefreshToken = async (req: Request, res: Response) => {
   const { cookie } = req.cookies;
   if (!cookie?.jwt) return res.status(401);
 
-  // const foundUser = userDB.users.find(
-  //   (user: User) => user.refreshToken === cookie.jwt
-  // );
-  // if (!foundUser) return res.sendStatus(403);
+  if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET) {
+    console.error(
+      "ACCESS_TOKEN_SECRET is not defined in environment variables",
+      process.env.ACCESS_TOKEN_SECRET,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    return res
+      .status(500)
+      .json({ message: "Something really bad happen. Visit us later." });
+  }
 
-  // if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET) {
-  //   console.error(
-  //     "ACCESS_TOKEN_SECRET is not defined in environment variables"
-  //   );
-  //   return res.sendStatus(500);
-  // }
+  jwt.verify(
+    cookie.jwt,
+    process.env.REFRESH_TOKEN_SECRET as string,
+    (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
+      if (err) return res.sendStatus(403);
 
-  // jwt.verify(
-  //   cookie.jwt,
-  //   process.env.REFRESH_TOKEN_SECRET as string,
-  //   (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
-  //     if (err) return res.sendStatus(403);
-
-  //     if (
-  //       !process.env.ACCESS_TOKEN_SECRET ||
-  //       !process.env.REFRESH_TOKEN_SECRET
-  //     ) {
-  //       console.error(
-  //         "ACCESS_TOKEN_SECRET is not defined in environment variables"
-  //       );
-  //       return res.sendStatus(500);
-  //     }
-
-  //     if (typeof decoded !== "string" && decoded?.userName) {
-  //       const accessToken = jwt.sign(
-  //         { userName: decoded.userName },
-  //         process.env.ACCESS_TOKEN_SECRET as string,
-  //         { expiresIn: "1m" }
-  //       );
-  //       res.json({ accessToken });
-  //     } else {
-  //       res.sendStatus(500);
-  //     }
-  //   }
-  // );
+      if (typeof decoded !== "string" && decoded?.email) {
+        const refreshToken = jwt.sign(
+          { email: decoded.email },
+          process.env.REFRESH_TOKEN_SECRET as string,
+          { expiresIn: "1d" }
+        );
+        res.cookie("jwt", refreshToken, {
+          httpOnly: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        });
+      } else {
+        res.sendStatus(500);
+      }
+    }
+  );
 };
