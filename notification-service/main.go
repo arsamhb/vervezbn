33f847151f8a654c7c3efa4c6b9338ec4d5f4170
Arsam <arsam.hb@gmail.com>
2024-07-29 16:59:33 +0330

    feat/database connection - env variables

--- notification-service/main.go
+++ notification-service/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"notification-service/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
-	fmt.Println("Hello verve")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
+
+// RUN COMMAND
+// CompileDaemon -command="./notification-service"
