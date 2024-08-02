b0a3400517e355daed0474f1da2a9650eb7297ba
Arsam <87975732+arsamhb@users.noreply.github.com>
2024-08-02 16:58:10 +0330

    feat/transaction validation middleware

---
+++ backend/src/middleware/transaction-validation-middleware.ts
@@ -0,0 +1,34 @@
+import { z, ZodError } from "zod";
+import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const validateTransactionChargeRequest = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!process.env.SINGLE_TOKEN_VALUE_IN_TOMAN) {
+    return res.status(500).json({
+      message: "something bad happened to us visit us a few moment later",
+    });
+  }
+
+  const chargeWalletTransactionSchema = z.object({
+    amount: z
+      .number()
+      .gt(parseInt(process.env.SINGLE_TOKEN_VALUE_IN_TOMAN as string)),
+    id: z.string(),
+  });
+
+  try {
+    chargeWalletTransactionSchema.parse(req.body);
+    next();
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({ errors: error.errors });
+    }
+    next(error);
+  }
+};
