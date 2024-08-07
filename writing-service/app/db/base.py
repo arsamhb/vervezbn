12ab6c2361145aceeeb86a402c2b5a20c795699d
aka-abdol <thegrolam@gmail.com>
2024-08-07 11:56:23 +0330

    feat: models configuration done

--- writing-service/app/db/base.py
+++ writing-service/app/db/base.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.models import BaseModel
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 DATABASE_URL = 'postgresql://postgres:secret@localhost:5431/vervez'
 engine = create_engine(DATABASE_URL, echo=True)
-Session = sessionmaker(autoflush=False, bind=engine)
+Session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+class BaseModel(DeclarativeBase):
+    pass
 
 def init_db():
     BaseModel.metadata.create_all(bind=engine)
--- writing-service/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-class BaseModel(DeclarativeBase):
-    pass
-
-from .examiner import Examiner
-from .writing import Writing
-from .writing_prompt import WritingPrompt
